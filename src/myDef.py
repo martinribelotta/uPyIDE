deaad9b725355fa642bef2961e03474890132766
coolshou <coolshou2060@gmail.com>
2017-07-30 21:50:21 +0800

    use locale.getdefaultlocale() to get lang

--- src/myDef.py
+++ src/myDef.py
@@ -6,7 +6,9 @@ Created on Tue Jul 25 00:22:16 2017
 @author: coolshou
 """
 import pyqode_i18n
+import locale
 
 def i18n(s):
-    return pyqode_i18n.tr(s, lang='zh_TW')
+    lang, encode = locale.getdefaultlocale()
+    return pyqode_i18n.tr(s, lang=lang)
 
